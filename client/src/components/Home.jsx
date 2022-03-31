11a1a991a233fa215861f9b140876ee6d584004b
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-03-31 16:16:19 -0400

    added more styling to Nav

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,9 +5,7 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div className="home-container">
-        <div className="b-img">
         <h1>Rentitfy</h1>
-        </div>
         <Link className="list-link" to='/listings'> Browse Listings</Link>
         <Link className="home-p" to="/contact">See something you like? Contact our agent now!</Link>
     </div>
