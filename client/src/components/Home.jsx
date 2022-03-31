b7d20f3c6f2b71afed67a31c876a0e56f76bf4db
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-03-31 14:12:39 -0400

    added styling to Home component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,9 +5,7 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div className="home-container">
-        <h1>
-        <span>R</span><span>e</span><span>n</span><span>t</span><span>i</span><span>t</span><span>f</span><span>y</span></h1>
-        <h3>Find your new home here!</h3>
+        <h1>Rentitfy</h1>
         <br></br>
         <Link className="list-link" to='/listings'> Browse Listings</Link>
         <br></br>
