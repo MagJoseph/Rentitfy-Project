f98dd6e4284aebfdf76aaf676063bb8714614ac6
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-03-31 15:17:56 -0400

    created a form in Contact component

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <h2>Contact</h2>
+      <p>Please leave us your contact information 
+           and our Agent will get back to you.</p>
+           <form>
+           <input type="text" placeholder={"First Name"} />
+           <input type="text" placeholder={"Last Name"} />
+           <input type="text" placeholder={"Phone Number"} />
+           <input type="text" placeholder={"Email Address"} />
+           <button>Submit</button>
+           </form>
+    </div>
   )
 }
 
